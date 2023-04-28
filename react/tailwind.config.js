a3c03f831c08ea4e6cedab77d82b8cc2d74ae71d
pvbiadmin <pvbi.org@gmail.com>
2023-04-28 12:36:34 +0800

    Added Tailwindui components

--- react/tailwind.config.js
+++ react/tailwind.config.js
@@ -1,13 +1,16 @@
+/* eslint-disable no-undef */
 /** @type {import('tailwindcss').Config} */
+
 export default {
-  purge: ['./index.html', './src/**/*.{vue,js,ts,jsx,tsx}'],
-  darkMode: false, // or 'media' or 'class'
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
   theme: {
     extend: {},
   },
-  variants: {
-    extend: {},
-  },
-  plugins: [],
+  plugins: [
+    require('@tailwindcss/forms')
+  ],
 }
 
