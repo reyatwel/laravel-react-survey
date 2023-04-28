7e0ce9b1ca3a7a45823ebb4e623871ba4ea09ef2
pvbiadmin <pvbi.org@gmail.com>
2023-04-28 08:34:38 +0800

    Added Heroicons

--- react/src/App.jsx
+++ react/src/App.jsx
@@ -1,3 +1,4 @@
+import { BeakerIcon } from '@heroicons/react/24/solid'
 import './App.css'
 
 function App() {
@@ -6,6 +7,7 @@ function App() {
       <h1 className="text-3xl font-bold underline bg-purple-500">
         Hello world!
       </h1>
+      <BeakerIcon className="h-6 w-6 text-blue-500" />
     </>
   )
 }
