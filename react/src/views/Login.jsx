b2cd5b8ed3fdf07c74ed25c51f6c5aff97db74c5
pvbiadmin <pvbi.org@gmail.com>
2023-04-28 17:20:51 +0800

    Install and configure React Router DOM

--- react/src/views/Login.jsx
+++ react/src/views/Login.jsx
@@ -1,5 +1,6 @@
 
-export default function App() {
+
+export default function Login() {
   return (
     <>
       <div className="flex min-h-full flex-1 flex-col justify-center px-6 py-12 lg:px-8">
@@ -69,3 +70,5 @@ export default function App() {
     </>
   )
 }
+
+
---
+++ react/src/views/Surveys.jsx
@@ -0,0 +1,9 @@
+
+export default function Surveys() {
+  return (
+    <div>
+      Surveys
+    </div>
+  )
+}
+
